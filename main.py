c7a0fcd9958f5bc2dc8156e12e2ff88a1dae8ad8
Felipe Jorquera <f.jorquera.uribe@gmail.com>
2016-09-20 00:28:38 -0300

    live face recognition ready

--- main.py
+++ main.py
@@ -19,8 +19,18 @@ my_recognizer.train(faces, labels)
 
 while True:
 	frame = webcam.get_frame()
-	faces_coord = my_detector.detect(frame)
+	faces_coord = my_detector.detect(frame, min_neighbors=10)
 	
+	if len(faces_coord):
+		faces = ut.normalize_faces(frame, faces_coord)
+
+		for i, face in enumerate(faces):
+			conf, pred = my_recognizer.predict(face, labels_dic)
+			ut.draw_rectangle_with_text(frame, (faces_coord[i][0], faces_coord[i][1]),\
+				( faces_coord[i][0]+faces_coord[i][2], faces_coord[i][1]+faces_coord[i][3] ), pred)
+		cv2.imshow('Tutorial', frame)
+
+	'''
 	for(x, y, w, h) in faces_coord:
 		ut.draw_rectangle_with_text(frame, (x,y), (x+w,y+h))
 
@@ -29,7 +39,7 @@ while True:
 	#cv2.imshow("Tutorial", faces[0])
 	
 	cv2.imshow("Tutorial", frame)
-
+	'''
 	# si se presiona ESC
 	if cv2.waitKey(20) & 0xFF==27:
 		break
